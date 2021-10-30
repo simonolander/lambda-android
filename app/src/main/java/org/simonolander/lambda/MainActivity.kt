23397057a57bdeb50ec062c82ff3fb4fd5ff2659
Simon Olander <simon.olander@r2m.se>
2021-10-30 15:42:55 +0200

    Add tutorial page 2

--- app/src/main/java/org/simonolander/lambda/MainActivity.kt
+++ app/src/main/java/org/simonolander/lambda/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.tutorial_page_1)
     }
 }
