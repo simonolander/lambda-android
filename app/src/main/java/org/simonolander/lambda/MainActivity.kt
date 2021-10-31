1072c478440f658833add8b3a9205a0d12d51bbd
Simon Olander <simon.olander@r2m.se>
2021-10-31 08:37:05 +0100

    Splash screen

--- app/src/main/java/org/simonolander/lambda/MainActivity.kt
+++ app/src/main/java/org/simonolander/lambda/MainActivity.kt
@@ -2,12 +2,14 @@ package org.simonolander.lambda
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.main)
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
