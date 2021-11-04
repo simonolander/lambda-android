dda52004cd61b1a3582e0797cb4bbfe5dcd372ee
Simon Olander <simon.olander@r2m.se>
2021-11-04 19:22:10 +0100

    Add function for c1l1

--- app/src/main/java/org/simonolander/lambda/MainActivity.kt
+++ app/src/main/java/org/simonolander/lambda/MainActivity.kt
@@ -1,12 +1,26 @@
 package org.simonolander.lambda
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Snackbar
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import org.simonolander.lambda.ui.theme.LambdaTheme
 
@@ -17,7 +31,7 @@ class MainActivity : ComponentActivity() {
             LambdaTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Chapter1Level1()
                 }
             }
         }
@@ -29,10 +43,40 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LambdaTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(name = "light mode", showBackground = true, uiMode = UI_MODE_NIGHT_NO)
+@Preview(name = "dark mode", showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun Chapter1Level1() {
+    val context = LocalContext.current
+    LambdaTheme {
+        Surface {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clickable {
+                        Toast.makeText(context, "hallå", Toast.LENGTH_SHORT).show()
+                    }
+            ) {
+                Text(
+                    text = "λx. x",
+                    style = MaterialTheme.typography.h2,
+                    textAlign = TextAlign.Center,
+                )
+                Text(
+                    text = "This is a function",
+                    style = MaterialTheme.typography.subtitle1,
+                    textAlign = TextAlign.Center,
+                )
+            }
+        }
     }
 }
