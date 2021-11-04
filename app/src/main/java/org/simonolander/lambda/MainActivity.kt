43ae49b3a680690d6c7686623574745a0c871d16
Simon Olander <simon.olander@r2m.se>
2021-11-04 19:36:02 +0100

    Add function for c1l2

--- app/src/main/java/org/simonolander/lambda/MainActivity.kt
+++ app/src/main/java/org/simonolander/lambda/MainActivity.kt
@@ -9,16 +9,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Snackbar
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,20 +36,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Composable
-fun DefaultPreview() {
-    LambdaTheme {
-        Greeting("Android")
-    }
-}
-
 @Preview(name = "light mode", showBackground = true, uiMode = UI_MODE_NIGHT_NO)
 @Preview(name = "dark mode", showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun DefaultPreview() {
+    Chapter1Level2()
+}
+
 @Composable
 fun Chapter1Level1() {
     val context = LocalContext.current
@@ -63,7 +54,9 @@ fun Chapter1Level1() {
                 modifier = Modifier
                     .fillMaxSize()
                     .clickable {
-                        Toast.makeText(context, "hallå", Toast.LENGTH_SHORT).show()
+                        Toast
+                            .makeText(context, "hallå", Toast.LENGTH_SHORT)
+                            .show()
                     }
             ) {
                 Text(
@@ -76,6 +69,49 @@ fun Chapter1Level1() {
                     style = MaterialTheme.typography.subtitle1,
                     textAlign = TextAlign.Center,
                 )
+            }
+        }
+    }
+}
+
+@Composable
+fun Chapter1Level2() {
+    val context = LocalContext.current
+    LambdaTheme {
+        Surface {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clickable {
+                        Toast
+                            .makeText(context, "hallå", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+            ) {
+                val functionPieces = listOf(
+                    "λ" to "λ starts the function" to MaterialTheme.colors.primary,
+                    "x" to "Parameters" to MaterialTheme.colors.secondary,
+                    ". " to "End of parameters" to MaterialTheme.colors.primaryVariant,
+                    "x" to "Function body" to MaterialTheme.colors.secondaryVariant,
+                )
+                Row {
+                    functionPieces.forEach {
+                        Text(
+                            text = it.first.first,
+                            color = it.second,
+                            style = MaterialTheme.typography.h2,
+                        )
+                    }
+                }
+                functionPieces.forEach {
+                    Text(
+                        text = it.first.second,
+                        color = it.second,
+                        style = MaterialTheme.typography.subtitle1,
+                    )
+                }
             }
         }
     }
