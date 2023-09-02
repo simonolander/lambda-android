970814cf25fee7566ec47c7f6b6c5f4ac4fdfb16
Simon Olander Sahlén <simon.olander-sahlen@regionstockholm.se>
2023-09-02 13:39:25 +0200

    Upgraded gradle things

---
+++ app/build.gradle.kts
@@ -0,0 +1,88 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "org.simonolander.lambda"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "org.simonolander.lambda"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+//    implementation "androidx.appcompat:appcompat:1.4.0"
+//    implementation "androidx.compose.material:material:$compose_version"
+//    implementation "androidx.compose.ui:ui-tooling-preview:$compose_version"
+//    implementation "androidx.compose.ui:ui:$compose_version"
+//    implementation "androidx.core:core-ktx:1.7.0"
+//    implementation "androidx.lifecycle:lifecycle-runtime-ktx:2.4.0"
+//    implementation "androidx.navigation:navigation-compose:2.4.0-beta02"
+//    implementation "androidx.compose.material:material-icons-extended:1.1.0-rc01"
+//    implementation "androidx.room:room-runtime:$room_version"
+//    implementation "androidx.room:room-ktx:$room_version"
+//    implementation "com.google.accompanist:accompanist-flowlayout:0.21.2-beta"
+//    implementation "com.google.android.material:material:1.4.0"
+//    implementation "org.jetbrains.kotlin:kotlin-reflect:1.6.0"
+//    ksp "androidx.room:room-compiler:$room_version"
+//    testImplementation "io.kotest:kotest-runner-junit5-jvm:$kotest_version"
+//    testImplementation "io.kotest:kotest-assertions-core-jvm:$kotest_version"
+//    testImplementation "io.kotest:kotest-property-jvm:$kotest_version"
+//    androidTestImplementation "androidx.compose.ui:ui-test-junit4:$compose_version"
+//    androidTestImplementation "androidx.test.espresso:espresso-core:3.4.0"
+//    androidTestImplementation "androidx.test.ext:junit:1.1.3"
+//    debugImplementation "androidx.compose.ui:ui-tooling:$compose_version"
+}
