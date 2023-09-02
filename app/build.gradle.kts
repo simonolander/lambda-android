7387cb9e7cc4d5b8925099d343a90d6e6cddbffe
Simon Olander Sahlén <simon.olander-sahlen@regionstockholm.se>
2023-09-02 18:21:11 +0200

    Use multiplatform version of kotest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,9 +72,9 @@ dependencies {
     ksp("androidx.room:room-compiler:2.5.2")
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.6.0")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:5.6.2")
-    testImplementation("io.kotest:kotest-property-jvm:5.6.0")
+    testImplementation("io.kotest:kotest-runner-junit5:5.6.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
+    testImplementation("io.kotest:kotest-property:5.6.0")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
